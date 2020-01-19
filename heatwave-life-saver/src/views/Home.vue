c7d368136d7e327748328c7cc83b017e4a08a5a2
noursen <houidhek.noursen@gmail.com>
2020-01-18 19:22:15 -0500

    changement home page

--- heatwave-life-saver/src/views/Home.vue
+++ heatwave-life-saver/src/views/Home.vue
@@ -1,27 +1,103 @@
 <template>
-    <div class="profile-page">
-        <section class="section-profile-cover section-shaped my-0">
-            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </section>
-        <section class="section section-skew">
+    <div>
+
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew">
+                   
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">Le Heatwave Guide
+                                    <span>Contrer les vagues de chaleurs en sauvant la planète</span>
+                                </h1>
+                                <p class="lead  text-white">La solution vous permet de combattre la chaleur de l'été en trouvant rapidement un abris climatisé ou des fontaines pour éviter l'achat des bouteilles en plastique</p>
+                                <div class="btn-wrapper">
+                                    
+                                    <base-button tag="a"
+                                                 href=""
+                                                 class="mb-3 mb-sm-0"
+                                                 type="white"
+                                                 icon="fa fa-map-o">
+                                        Commencez maintenant
+                                    </base-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+        </div>
+        <section class="section section-lg pt-lg-0 mt--200">
             <div class="container">
-                <card shadow class="card-profile mt--300" no-body>
-                    Home page, have fun ;)
-                </card>
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="row row-grid">
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="fa fa-tint" type="primary" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-primary text-uppercase">Go-Fontaine</h6>
+                                    <p class="description mt-3">vous avez soif et vous voulez remplir votre gourde, faites 
+                                        le maintenant en localisant la fontaine la plus proche</p>
+                                    <div>
+                                        <badge type="primary" rounded>Map</badge>
+                                        <badge type="primary" rounded>Eau</badge>
+                                        <badge type="primary" rounded>Recherche</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="primary" class="mt-4">
+                                        Top-Départ
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="fa fa-snowflake-o" type="success" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-success text-uppercase">Go-Cool</h6>
+                                    <p class="description mt-3">Le soleil tape fort ? vous voulez être à l'abri? cherchez maintenant l'endroit public climatisé le plus proche .</p>
+                                    <div>
+                                        <badge type="success" rounded>Chaleur</badge>
+                                        <badge type="success" rounded>Climatiseur</badge>
+                                        <badge type="success" rounded>Protection</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="success" class="mt-4">
+                                        Top-Départ
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-bell-55" type="warning" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-warning text-uppercase">Consulter les alertes</h6>
+                                    <p class="description mt-3">En un seul clic, ayez accès aux avertissements météorologiques les plus récents </p>
+                                    <div>
+                                        <badge type="warning" rounded>Météo</badge>
+                                        <badge type="warning" rounded>Alertes</badge>
+                                        <badge type="warning" rounded>Clic</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="warning" class="mt-4">
+                                        Top-Départ
+                                    </base-button>
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
-    </div>
+     </div>
 </template>
 <script>
-export default {};
+
+export default {
+  
+  };
 </script>
 <style>
 </style>
